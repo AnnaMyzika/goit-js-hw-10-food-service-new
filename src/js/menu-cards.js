b8b9ebaddd09e603e372734dc1456b8d31c83f7e
Anna Myzika <myzikaanya@gmail.com>
2021-03-28 20:24:03 +0300

    добавлена разметка и изменение темы

---
+++ src/js/menu-cards.js
@@ -0,0 +1,70 @@
+import data from '../data/menu.json';
+import template from '../template/template.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    body: document.body,
+    list: document.querySelector('.js-menu'),
+    input: document.querySelector('.theme-switch__toggle'),
+}
+
+// refs.body.classList.add(localStorage.getItem('theme') === null ? Theme.LIGHT : localStorage.getItem('theme'));
+// refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
+
+// console.log(refs);
+
+// const markup = template(data);
+// refs.list.insertAdjacentHTML('beforeend', markup);
+
+// function chenchTheme(add, rem) {
+//      refs.body.classList.remove(rem);
+//         refs.body.classList.add(add);
+//     localStorage.setItem('theme', add);
+// }
+
+// refs.input.addEventListener('change', inputChangeOn);
+// function inputChangeOn({target}) {
+//     if (target.checked) {
+//         chenchTheme(Theme.DARK, Theme.LIGHT);
+//     }
+
+//     else {
+//         chenchTheme(Theme.LIGHT, Theme.DARK);
+//     }
+// }
+
+
+refs.body.classList.add(localStorage.getItem('theme') === null ? Theme.LIGHT : localStorage.getItem('theme'));
+refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
+// refs.body.classList.add(Theme.LIGHT);
+
+console.log(refs);
+
+const markup = template(data);
+refs.list.insertAdjacentHTML('beforeend', markup);
+
+function chenchTheme(add, rem) {
+     refs.body.classList.remove(rem);
+        refs.body.classList.add(add);
+    localStorage.setItem('theme', add);
+}
+
+refs.input.addEventListener('change', inputChangeOn);
+function inputChangeOn({target}) {
+    if (target.checked) {
+        chenchTheme(Theme.DARK, Theme.LIGHT);
+    }
+
+    else {
+        chenchTheme(Theme.LIGHT, Theme.DARK);
+    }
+    // target.checked ? refs.body.classList.add(Theme.DARK) :     
+}
+
+
+
+
